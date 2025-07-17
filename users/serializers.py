f42f50e4393761bc4e4da45a1947dfb42a351dfd
Anshika Rai <anshikacoder10@gmail.com>
2025-07-17 12:58:02 +0530

    Setup custom user model with registration API

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,33 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.tokens import RefreshToken
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
 
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'name', 'password', 'password2')
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Password must match")
+        return data
+    
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = CustomUser.objects.create_user(**validated_data)
+
+        refresh = RefreshToken.for_user(user)
+        token_data = {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+
+        return {
+            'email':user.email,
+            'name':user.name,
+            'token': token_data
+        }
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+from .models import CustomUser
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    # queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    # permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user_data = serializer.save()
+        return Response(user_data, status=status.HTTP_201_CREATED)
