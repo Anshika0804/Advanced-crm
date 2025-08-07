96f00f0a84cff07d8612e0b45089816bb0c90984
Anshika Rai <anshikacoder10@gmail.com>
2025-08-07 19:41:28 +0530

    Team Crud operations

--- teams/urls.py
+++ teams/urls.py
@@ -10,3 +10,11 @@ urlpatterns = [
     path('<int:pk>/', TeamRetrieveUpdateDestroyView.as_view(), name='team-detail'),
     path('with-users/', TeamWithUsersListView.as_view(), name='team-with-users'),
 ]
+
+
+from .views import assign_users_to_team, remove_users_from_team
+
+urlpatterns += [
+    path('<int:team_id>/assign-users/', assign_users_to_team, name='assign-users-to-team'),
+    path('<int:team_id>/remove-users/', remove_users_from_team, name='remove-users-from-team'),
+]
