3d757350a4cdb9e66e2d11a8d99d26279734d42f
Anshika Rai <anshikacoder10@gmail.com>
2025-09-02 18:25:49 +0530

    Updation in Procfile and requirement

--- crm_project/asgi.py
+++ crm_project/asgi.py
@@ -1,10 +1,5 @@
 """
 ASGI config for crm_project project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
@@ -12,11 +7,11 @@ import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-import notifications.routing
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crm_project.settings")
-django.setup()
+django.setup()  # must be before importing your routing
+
+import notifications.routing  # <-- import AFTER django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- notifications/consumers.py
+++ notifications/consumers.py
@@ -1,55 +1,26 @@
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer 
-from channels.db import database_sync_to_async 
-from urllib.parse import parse_qs
-from django.contrib.auth.models import AnonymousUser
-# from django.conf import crm_project.authentication import get_user_from_token  
+from channels.generic.websocket import AsyncWebsocketConsumer
+
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        #TOKEN VALIDATION
-        # query_string = self.scope["query_string"].decode()  # bytes -> string
-        # params = parse_qs(query_string)
-        # token_list = params.get("token", [])
-
-        # if not token_list:
-        #     await self.close(code=4001)  # token missing
-        #     return
-
-        # token = token_list[0]
-
-        # # Validate token and get user
-        # self.user = await get_user_from_token(token)
-        # if self.user is None or isinstance(self.user, AnonymousUser):
-        #     await self.close(code=4001)  # invalid token
-        #     return
-
-
-        # AuthMiddlewareStack automatically associates scope["user"] based on Django’s session cookies.
+        # If you use AuthMiddlewareStack, user is automatically added to scope
         if self.scope["user"].is_anonymous:
             await self.close()
         else:
             self.user = self.scope["user"]
             self.group_name = f"user_{self.user.id}"
+
+            # Add this connection to the user-specific group
             await self.channel_layer.group_add(self.group_name, self.channel_name)
             await self.accept()
 
-
     async def disconnect(self, close_code):
+        # Remove from group on disconnect
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def send_notification(self, event):
+        # Handler when a notification is sent via group_send
         message = event['message']
-        print(f"Sending WS message to user {self.user.id}: {message}")  
+        print(f"Sending WS message to user {self.user.id}: {message}")
         await self.send(text_data=json.dumps(message))
-        # handler method invoked by the channel layer when someone calls:
-        # await channel_layer.group_send(
-        #     f"user_{user_id}",
-        #     {"type": "send_notification", "message": {...}}
-        # )
-
-    # @database_sync_to_async
-    # def create_notification(self, lead, message):
-    #     return Notification.objects.create(user=self.user, lead=lead, message=message)
-
-    
